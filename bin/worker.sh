4ad6a62684e5cd4a87edc956644876bf6c0b020f
Katie Dillon <kedillon@umich.edu>
2019-11-01 19:19:07 -0400

    fix: removed AO framework

--- bin/worker.sh
+++ bin/worker.sh
@@ -5,7 +5,13 @@
 # See https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 set -Eeuo pipefail
 
+export PYTHONPATH=.
+
 # Worker just runs self play
 while true; do
   python simulator/run.py
+
+  if [ -e stop_running ]; then
+    exit
+  fi
 done
